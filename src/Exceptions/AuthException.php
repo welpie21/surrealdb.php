f89b6adc468191c9ed7cc50136865a1903ca7e51
Beau <beaudenheijer@gmail.com>
2024-04-17 23:33:15 +0200

    change: renamed AuthException to SurrealAuthException in the message

--- src/Exceptions/AuthException.php
+++ src/Exceptions/AuthException.php
@@ -8,6 +8,6 @@ class AuthException extends Exception
 {
     public function __construct(string $message)
     {
-        parent::__construct("AuthException: " . $message, 401);
+        parent::__construct("SurrealAuthException: " . $message, 401);
     }
 }
