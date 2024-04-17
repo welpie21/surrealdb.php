be3bcfda23e48587c5faea3c6bae6e3621f66313
Beau <beaudenheijer@gmail.com>
2024-04-17 23:34:26 +0200

    chore: added Cbor RecordID
    - added decoding and encoding a record
    - added fromArray function on RecordId class

--- src/Cbor/CBOR.php
+++ src/Cbor/CBOR.php
@@ -4,8 +4,11 @@ namespace Surreal\Cbor;
 
 use Beau\CborPHP\CborDecoder;
 use Beau\CborPHP\CborEncoder;
+use Beau\CborPHP\classes\TaggedValue;
 use Beau\CborPHP\exceptions\CborException;
+use Beau\CborPHP\utils\CborByteString;
 use Exception;
+use Surreal\Cbor\Types\RecordId;
 
 class CBOR
 {
@@ -17,21 +20,34 @@ class CBOR
      */
     public static function encode(mixed $data): ?string
     {
-        return CborEncoder::encode($data, function($key, $value) {
-			return $value;
-		});
+        return CborEncoder::encode($data, function ($key, $value) {
+
+            if ($value instanceof RecordId) {
+                return new TaggedValue(8, (string)$value);
+            }
+
+            return $value;
+        });
     }
 
     /**
-	 * Decodes CBOR data
+     * Decodes CBOR data
      * @param string $data
      * @return mixed
      * @throws Exception
      */
     public static function decode(string $data): mixed
     {
-        return CborDecoder::decode($data, function($key, $value) {
-			return $value;
-		});
+        return CborDecoder::decode($data, function ($key, $value) {
+
+            if(!($value instanceof TaggedValue)) {
+                return $value;
+            }
+
+            return match ($value->tag) {
+                8 => RecordId::fromArray($value->value),
+                default => throw new CborException("Unknown tag: " . $value->tag)
+            };
+        });
     }
 }
